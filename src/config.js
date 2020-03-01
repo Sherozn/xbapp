4ec1051fcec7c23eedc5413d5618bde8b01c2cef
xzn <xzn@stonescloud.com>
2020-03-01 20:30:26 +0800

    shangxian

--- src/config.js
+++ src/config.js
@@ -6,7 +6,6 @@ const config = {
   host
 }
 
-// 我们在【ES6知识点详解】中讲过export的意思是向外暴露接口
 // 这样在其他文件中可以通过import config from '@/config'引用
 // 然后通过config.loginUrl就能得到在本文件中配置的值'http://localhost:5757/weapp/login'
 export default config
