6feb29b932202799d626f0682d63cfc20295009f
xzn <xzn@stonescloud.com>
2020-03-02 17:06:42 +0800

    pub

--- config/index.js
+++ config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/data':{
+            target: 'http://lgshuolicai.com:3303',
+            changeOrigin: true
+        }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
