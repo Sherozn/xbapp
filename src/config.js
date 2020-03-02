57642b73c1acb124ad9ae046b8843d93772aa0bc
xzn <xzn@stonescloud.com>
2020-03-02 18:03:30 +0800

    dist

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // 域名，通过域名来找到服务器，在这里我们将本地电脑当成服务器
 // 所以现在配置的域名是本地的，当项目实际上线，这里需要换成实际的已备案的域名
-const host = 'http://lgshuolicai.com'
+const host = 'http://lgshuolicai.com:3303'
 
 const config = {
   host
