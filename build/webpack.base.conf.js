75967b65f9a0c4b03f30bc61b170179f815bb9cb
xzn <xzn@stonescloud.com>
2020-03-04 01:01:46 +0800

    push

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -15,6 +15,12 @@ module.exports = {
   entry: {
     app: './src/main.js'
   },
+  externals: {
+    'vue': 'Vue',
+    // 'vue-router': 'VueRouter',
+    'element-ui': 'ElementUI',
+    // 'vuex':'Vuex'
+  },
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import Vue from 'vue'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
 import store from './store.js'
 import './assets/style/border.css'
 import './assets/style/reset.css'
-import './assets/iconfont/iconfont.css'
+// import './assets/iconfont/iconfont.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
 Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 Vue.config.productionTip = false
