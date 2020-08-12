86b7c00acb7f700bb6fdd4e55fc22875680a95f9
xzn <xzn@stonescloud.com>
2020-08-12 17:25:12 +0800

    push

--- server/config/appid.js
+++ server/config/appid.js
@@ -7,6 +7,6 @@ module.exports = {
         appid: 'wx144003c81a20afee',
         secret: '1689b1e448c03ff2417afb5bb0a2dacc',
         encodingAESKey: 'YrpmTxUDRCZokovuPSWRkIcvZNOdnD7peVybIvNYbc3',
-        serverUrl: 'http://lgshuolicai.com:80'
+        serverUrl: 'http://lgshuolicai.com'
     }
 }
--- server/controller/gzh.js
+++ server/controller/gzh.js
@@ -171,15 +171,16 @@ class gzhController {
   static async getCode(ctx) {
     console.log("我进来了")
     const appid = config.wx.appid;
-    console.log("appid getCode",appid)
     const redirect_uri = urlencode(config.wx.serverUrl + "/brokerList"); //这里的url需要转为加密格式，它的作用是访问微信网页鉴权接口成功后微信会回调这个地址，并把code参数带在回调地址中
+    console.log("brokerList ",config.wx.serverUrl + "/brokerList")
+    console.log("redirect_uri ",redirect_uri)
     const scope = 'snsapi_base';
     const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=123&connect_redirect=1#wechat_redirect`;
 
     console.log("url",url)
     try{
-      const resStr = await axios.post(url)
-      // const resStr = ctx.redirect(url)
+      // const resStr = await axios.post(url)
+      const resStr = ctx.redirect(url)
       // ctx.body = {
       //   data:resStr
       // }
