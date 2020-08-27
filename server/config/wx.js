e706bb5696339708fae8330820cd39142e35c56c
xzn <xzn@stonescloud.com>
2020-08-27 16:48:48 +0800

    push

--- server/config/wx.js
+++ server/config/wx.js
@@ -55,7 +55,7 @@ exports.message = {
                             item:{
                                 Title:"你好",
                                 Description:"我也好",
-                                PicUrl:"/static/img/预约顾问.jpg",
+                                PicUrl:"https://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=%E5%9B%BE%E7%89%87%E5%95%8A&hs=2&pn=1&spn=0&di=150040&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&ie=utf-8&oe=utf-8&cl=2&lm=-1&cs=3094443603%2C1387896325&os=1861416339%2C887580298&simid=4239883631%2C744530036&adpicid=0&lpn=0&ln=30&fr=ala&fm=&sme=&cg=&bdtype=0&oriquery=%E5%9B%BE%E7%89%87%E5%95%8A&objurl=http%3A%2F%2Fgss0.baidu.com%2F-Po3dSag_xI4khGko9WTAnF6hhy%2Fzhidao%2Fpic%2Fitem%2F6f061d950a7b0208a0864dbb62d9f2d3572cc819.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fr5oj6_z%26e3Bkwt17_z%26e3Bv54AzdH3Fq7jfpt5gAzdH3Fnab8adnm0_z%26e3Bip4s%3Fqks%3D6jswpj_q7jfpt5g_m&gsm=2&islist=&querylist=",
                                 Url:"http://mp.weixin.qq.com/s?__biz=MzIwMDUxOTE5OA==&mid=100000005&idx=1&sn=47e6091cb32d88b8ce2326f0fcd0d93f&chksm=16faba74218d33626c641727c796c0b263a434e6d343a5b8e04722a83c20098350adeeb621d8#rd"
                             }
                         }
